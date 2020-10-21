debe83fae5dbdfe97b7c122780ece20401ae0704
blakerom <blakeromero001@gmail.com>
2020-10-21 11:50:51 -0700

    depth first traversal with recursion

--- code-challenges/depthFirst/depthFirst.js
+++ code-challenges/depthFirst/depthFirst.js
@@ -1,1 +1,28 @@
+'use strict'
 
+const { Graph } = require('../graph/graph')
+
+class DepthFirst extends Graph {
+  constructor() {
+    super()
+  }
+
+  depthFirstTraversal(node, visited = new Set()) {
+    visited.add(start)
+    let collection = [];
+
+    const destinations = adjacencyList.get(node);
+    //looping over the edges of the adjacency list
+    for (const destination of destinations) {
+      if(!visited.has(destination)) {
+        collection.push(destination.value);
+        this.depthFirstTraversal(destination, visited);
+      }
+    }
+  }
+
+}
+
+module.exports = DepthFirst;
+
+// thanks to https://www.youtube.com/watch?v=cWNEl4HE2OE
